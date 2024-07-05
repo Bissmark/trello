e02a52d2e11b4d89da64f9ed3a3c1a18f957f708
Christopher Holt <holt.christopher1@gmail.com>
2024-07-05 21:13:08 +1000

    setting up tailwind, home, navbar, listitem pages

---
+++ frontend/src/components/ListItem.jsx
@@ -0,0 +1,9 @@
+const ListItem = ({ item }) => {
+    return (
+        <div>
+            <p>{item}</p>
+        </div>
+    )
+}
+
+export default ListItem;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,43 @@
 import { useState } from 'react';
+import ListItem from '../components/ListItem';
 
 const Home = () => {
+    const [addingList, setAddingList] = useState(false);
+    const [listName, setListName] = useState('');
     const [lists, setLists] = useState([]);
+
+    const _handleAddList = () => {
+        console.log('Adding List:', listName);
+        setLists([...lists, listName]);
+        setAddingList(false);
+        setListName('');
+    }
 
     return (
         <div className='flex flex-row flex-start'>
-            <h1>Home</h1>
-            <p>Welcome to the Home page</p>
+            <div>
+                {lists.map((item, index) => (
+                    <div key={index}>
+                        <ListItem item={item} />
+                    </div>
+                ))}
+            </div>
+            <div>
+                {!addingList ? (
+                    <button onClick={() => setAddingList(true)}>Add List</button>
+                ) : (
+                    <div>
+                        <input
+                            type='text'
+                            value={listName}
+                            onChange={(e) => setListName(e.target.value)}
+                        />
+                        <button onClick={_handleAddList}>Save</button>
+                        <button onClick={() => setAddingList(false)}>Cancel</button>
+                    </div>
+                
+                )}
+            </div>
         </div>
     )
 }
